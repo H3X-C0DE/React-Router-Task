db7700720e34a91f1e8897e4c8dfb25630fc5f42
IIHEXYII <vetletessem@gmail.com>
2022-12-19 16:19:21 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Libraries
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+// Routes
+import LandingPage from "./routes";
+import HorsePage from "./routes/HorsePage/Horse";
+import CatsPage from "./routes/CatsPage/Cats";
+import Page404 from "./routes/404/404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />}>
+          <Route path="/Horse" element={<HorsePage />} />
+          <Route path="/Cats" element={<CatsPage />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Router>
   );
 }
 
